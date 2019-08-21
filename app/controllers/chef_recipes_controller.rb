0d6a2153ac6c179f6ee48cfb65925b0c2716cb96
RPraxedes <richard0praxedes@gmail.com>
2019-08-21 11:41:46 +0800

    Add new recipe creation with integration tests

--- app/controllers/chef_recipes_controller.rb
+++ app/controllers/chef_recipes_controller.rb
@@ -2,4 +2,30 @@ class ChefRecipesController < ApplicationController
   def index
     @recipes = ChefRecipe.all
   end
+
+  def show
+    @recipe = ChefRecipe.find(params[:id])
+  end
+
+  def new
+    @recipe = ChefRecipe.new
+  end
+
+  def create
+    @recipe = ChefRecipe.new(recipe_params)
+    @recipe.chef_profile = ChefProfile.first  # no user auth yet
+    if @recipe.save
+        flash[:success] = "Recipe was created successfully!"
+        redirect_to chef_recipe_path(@recipe)
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def recipe_params
+    params.require(:chef_recipe).permit(:name, :description)
+  end
+
 end
--- test/integration/chef_recipes_test.rb
+++ test/integration/chef_recipes_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class ChefRecipesText < ActionDispatch::IntegrationTest
   def setup
     @chef = ChefProfile.create(chefname: "name", email: "mail@example.com")
-    @recipe = ChefRecipe.create(name: "veg", description: "veggies")
+    @recipe = ChefRecipe.create(name: "veg", description: "veggies", chef_profile_id: @chef.id)
     @recipe2 = @chef.chef_recipes.build(name: "chicken", description: "chicken")
     @recipe2.save
   end
@@ -16,7 +16,39 @@ class ChefRecipesText < ActionDispatch::IntegrationTest
   test "should get recipes listing" do
     get chef_recipes_path
     assert_template 'chef_recipes/index'
+    assert_select "a[href=?]", chef_recipe_path(@recipe), text: @recipe.name
+    assert_select "a[href=?]", chef_recipe_path(@recipe2), text: @recipe2.name
+  end
+
+  test "should get recipes show" do
+    get chef_recipe_path(@recipe)
+    assert_template 'chef_recipes/show'
     assert_match @recipe.name, response.body
-    assert_match @recipe2.name, response.body
+    assert_match @recipe.description, response.body
+    assert_match @chef.chefname, response.body
+  end
+
+  test "create new valid recipe" do
+    get new_chef_recipe_path
+    assert_template 'chef_recipes/new'
+    name_of_recipe = "Chicken"
+    description_of_recipe = "Just raw chicken"
+    assert_difference 'ChefRecipe.count', 1 do
+      post chef_recipes_path, params: { chef_recipe: {name: name_of_recipe, description: description_of_recipe} }
+    end
+    follow_redirect!
+    assert_match name_of_recipe.capitalize, response.body
+    assert_match description_of_recipe, response.body
+  end
+
+  test "reject new invalid recipes" do
+    get new_chef_recipe_path
+    assert_template 'chef_recipes/new'
+    assert_no_difference 'ChefRecipe.count' do
+      post chef_recipes_path, params: { chef_recipe: {name: " ", description: " "} }
+    end
+    assert_template 'chef_recipes/new'
+    assert_select 'p.alert-body'  # warning message
+    assert_select 'div.alert'
   end
 end
